12b0448bbbefbf2de091651ebbadeac3d3c948ec
Asus <dentanddent92@gmail.com>
2022-08-08 21:35:22 +0300

    class1

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-import sqlite3
+import telebot
+from telebot.types import Message
+from config import token
 
-sqlite3.connect('data.db')
+bot = telebot.TeleBot(token)
+@bot.message_handler()
+def func(message: Message):
+    bot.send_message(message.chat.id, message.text)
+
+bot.infinity_polling()
